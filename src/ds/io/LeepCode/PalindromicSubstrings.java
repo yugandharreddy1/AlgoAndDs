36a013b2bcf3e1e27b10ebc6f6f332dfe72d2d7d
yugandvo <yugandvo@YUGANDVO01.mshome.net>
2020-07-20 14:55:43 +0530

    PalindromicSubstrings

---
+++ src/ds/io/LeepCode/PalindromicSubstrings.java
@@ -0,0 +1,21 @@
+package ds.io.LeepCode;
+
+public class PalindromicSubstrings {
+	public static void main(String[] args) {
+		String s="aaa";
+		countSubstrings(s);
+	}
+	public static int countSubstrings(String s) {
+		int count = 0;
+		for (int i = 0; i < s.length(); i++)
+			count += countSubstrings(s, i, i) + countSubstrings(s, i, i + 1);
+		return count;
+	}
+
+	private static int countSubstrings(String s, int start, int end) {
+		int count = 0;
+		while (start >= 0 && end < s.length() && s.charAt(start--) == s.charAt(end++)) 
+			count++;
+		return count;
+	}
+}
