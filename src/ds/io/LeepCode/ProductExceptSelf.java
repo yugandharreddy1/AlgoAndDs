52d717f4b86ec88053a63fc293a5142ba325d27b
yugandvo <yugandvo@YUGANDVO01.mshome.net>
2020-07-20 15:40:38 +0530

    ProductExceptSelf

---
+++ src/ds/io/LeepCode/ProductExceptSelf.java
@@ -0,0 +1,28 @@
+package ds.io.LeepCode;
+
+public class ProductExceptSelf {
+	public static void main(String[] args) {
+		int[] nums=new int[]{1,2,3,4};
+		productExceptSelf(nums);
+	}
+	public static int[] productExceptSelf(int[] nums) {
+		int[] result=new int[nums.length];
+
+		for(int i=0;i<nums.length;i++){
+			result=prod(nums,result,i);
+		}
+
+		return result;
+	}
+
+	public static int[] prod(int[] nums,int[] result,int k){
+		int prod=1;
+		for(int i=0;i<nums.length;i++){
+			if(i!=k)
+				prod=prod*nums[i];
+		}
+		result[k]=prod;
+
+		return result;
+	}
+}
