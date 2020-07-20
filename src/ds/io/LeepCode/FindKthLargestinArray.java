7a3c9f103d469116b52c5b6e542bbf766f8bd5a3
yugandvo <yugandvo@YUGANDVO01.mshome.net>
2020-07-20 11:09:52 +0530

    FindKthLargestinArray

---
+++ src/ds/io/LeepCode/FindKthLargestinArray.java
@@ -0,0 +1,16 @@
+package ds.io.LeepCode;
+
+import java.util.Arrays;
+
+public class FindKthLargestinArray {
+	public static void main(String[] args) {
+		int[] nums=new int[]{3,2,3,1,2,4,5,5,6};
+		int k=2;
+		findKthLargest(nums,k);
+	}
+	public static int findKthLargest(int[] nums, int k) {
+		Arrays.sort(nums);
+
+		return nums[nums.length-k];
+	}
+}
