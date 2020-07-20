506299f8371ac9f3eed1551d2845772e2cc11820
yugandvo <yugandvo@YUGANDVO01.mshome.net>
2020-07-20 12:38:31 +0530

    MaxProductofArrayComment

--- src/ds/io/LeepCode/MaxProductofArray.java
+++ src/ds/io/LeepCode/MaxProductofArray.java
@@ -11,6 +11,11 @@ public class MaxProductofArray {
 			return 0;
 		}
 		int max = A[0], min = A[0], result = A[0];
+		
+		//Loop through the array, each time remember the max and min value for the previous product,
+		//the most important thing is to update the max and min value: we have to compare among max * A[i], min * A[i] as well as A[i], 
+		//since this is product, a negative * negative could be positive.
+		
 		for (int i = 1; i < A.length; i++) {
 			int temp = max;
 			max = Math.max(Math.max(max * A[i], min * A[i]), A[i]);
