0ce57213224cb6d1c1714600d15fe5c459686833
yugandvo <yugandvo@YUGANDVO01.corp.amdocs.com>
2020-07-23 18:52:00 +0530

    LongestSubstringWithoutRepeatingCharacters

--- src/ds/io/LeetCode/TwoSum.java
+++ src/ds/io/LeetCode/TwoSum.java
@@ -1,20 +1,24 @@
 package ds.io.LeetCode;
 
+import java.util.Arrays;
+
 public class TwoSum {
 
 	public static void main(String[] args) {
-		int nums[] =  {3,2,4};
-		int target = 6;
+		int nums[] = new int[] {2,5,5,11};
+		int target = 10;
+		StringBuffer sb=new StringBuffer();
+		char[] ch=new char[]{};
+		sb.toString().contains(String.valueOf(ch[0]));
 		System.out.println(twoSum(nums,target));
 	}
 	
 	 public static int[] twoSum(int[] nums, int target) {
 		 
-		 int temp[]=nums;
-		 int i;
-		 int j;
-		 for(i=0;i<nums.length;i++) {
-			 for(j=1;j<temp.length;j++) {
+		int[] temp=nums;
+		 //O(n2) Solution
+		 for(int i=0;i<nums.length;i++) {
+			 for(int j=1;j<temp.length;j++) {
 				 if(nums[i]==temp[j]) {
 					 break; 
 				 }
@@ -24,6 +28,7 @@ public class TwoSum {
 				 }
 			 } 
 		 }
+		 
 		return temp;
 		 
 	        
