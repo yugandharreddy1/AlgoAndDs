19d9a43dce3838404a838fd2fe48239f0890a74e
yugandvo <yugandvo@YUGANDVO01.corp.amdocs.com>
2020-07-24 23:38:56 +0530

    RemoveFromLinkedList

---
+++ src/ds/io/LeetCode/RemoveFromLinkedList.java
@@ -0,0 +1,24 @@
+package ds.io.LeetCode;
+
+public class RemoveFromLinkedList {
+	public static void main(String[] args) {
+
+	}
+	public ListNode removeElements(ListNode head, int val) { 
+		while(head != null && head.val == val) {
+			head = head.next;
+		}
+		if(head == null) {
+			return head;
+		}
+		ListNode p = head;
+		while(p.next != null) {
+			if(p.next.val == val) {
+				p.next = p.next.next;
+			} else {
+				p = p.next;
+			}
+		}
+		return head;
+	}
+}
