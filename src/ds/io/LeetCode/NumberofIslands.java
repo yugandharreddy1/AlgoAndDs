6e011a2d0d8b7022a753221623a3b654ac13d510
yugandvo <yugandvo@YUGANDVO01.corp.amdocs.com>
2020-08-17 23:01:23 +0530

    NumberofIslands

---
+++ src/ds/io/LeetCode/NumberofIslands.java
@@ -0,0 +1,32 @@
+package ds.io.LeetCode;
+
+public class NumberofIslands {
+
+	public static void main(String[] args) {
+		char[][] grid = new char[][]{
+			{'1','1','1','1','0'},
+			{'1','1','0','1','0'},
+			{'1','1','0','0','0'},
+			{'0','0','0','0','0'}
+		};
+		numIslands(grid);
+	}
+	
+	static char[][] g;
+    public static int numIslands(char[][] grid) {
+        int islands = 0;
+        g = grid;
+        for (int i=0; i<g.length; i++)
+            for (int j=0; j<g[i].length; j++)
+                islands += sink(i, j);
+        return islands;
+    }
+    static int sink(int i, int j) {
+        if (i < 0 || i == g.length || j < 0 || j == g[i].length || g[i][j] == '0')
+            return 0;
+        g[i][j] = '0';
+        sink(i+1, j); sink(i-1, j); sink(i, j+1); sink(i, j-1);
+        return 1;
+    } 
+
+}
