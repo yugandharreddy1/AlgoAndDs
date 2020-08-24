d95a8b37fc063ed11aae126285d941c3cb76ebc7
yugandvo <yugandvo@YUGANDVO01.mshome.net>
2020-08-24 13:31:23 +0530

    WordLadder

---
+++ src/ds/io/LeetCode/WordLadder.java
@@ -0,0 +1,58 @@
+package ds.io.LeetCode;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class WordLadder {
+
+	public static void main(String[] args) {
+		String beginWord = "hit",endWord = "cog";
+		String[] list={"hot","dot","dog","lot","log","cog"};
+		List<String> wordList =Arrays.asList(list);
+		ladderLength(beginWord,endWord,wordList);
+	}
+	public static int ladderLength(String beginWord, String endWord, List<String> wordList) {
+		Set<String> wordSet = new HashSet<String>(wordList);
+		if( !wordSet.contains(endWord) )
+			return 0;
+		// 3. Use set instead of queue during bfs
+		Set<String> forwardSet = new HashSet<String>(); 
+		Set<String> backwardSet = new HashSet<String>();
+		forwardSet.add(beginWord);
+		backwardSet.add(endWord);
+		wordSet.remove(endWord);
+		wordSet.remove(beginWord);
+		// 1. Search from entry and exit simultaneously
+		return transform(forwardSet, backwardSet, wordSet);
+	}
+
+	public static int transform(Set<String> forwardSet, Set<String> backwardSet, Set<String> wordSet) {
+		Set<String> newSet = new HashSet<String>();
+		for(String fs : forwardSet) {
+			char wordArray[] = fs.toCharArray();
+			for(int i = 0; i < wordArray.length; i++) {
+				for(int c = 'a'; c <= 'z'; c++) {
+					char origin = wordArray[i];
+					wordArray[i] = (char) c;
+					String target = String.valueOf(wordArray);
+					if( backwardSet.contains(target) )
+						return 2; // stop bfs when entry and exits meet
+						else if( wordSet.contains(target) && !forwardSet.contains(target) ) {
+							wordSet.remove(target); // 4. Remove visited word from wordList to decrease the search time
+							newSet.add(target);
+						}
+					wordArray[i] = origin;
+				}
+			}
+		}
+		if( newSet.size() == 0 )
+			return 0;
+		forwardSet = newSet;
+		// 2. Pick the queue with less elements to bfs
+		int result = forwardSet.size() > backwardSet.size() ? 
+				transform(backwardSet, forwardSet, wordSet) : transform(forwardSet, backwardSet, wordSet);
+				return result == 0 ? 0 : result + 1;
+	}
+}
