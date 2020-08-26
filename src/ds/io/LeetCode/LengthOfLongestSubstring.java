10570c6859c925eb6b6a84a729a3a7d7826b0469
yugandvo <yugandvo@YUGANDVO01.mshome.net>
2020-08-26 21:16:59 +0530

    LengthOfLongestSubstring

---
+++ src/ds/io/LeetCode/LengthOfLongestSubstring.java
@@ -0,0 +1,26 @@
+package ds.io.LeetCode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LengthOfLongestSubstring {
+	public static void main(String[] args) {
+		String s="abcabcbb";
+		System.out.println(lengthOfLongestSubstring(s));
+	}
+	public static int lengthOfLongestSubstring(String s) {
+		int i = 0, j = 0, max = 0;
+		Set<Character> set = new HashSet<>();
+
+		while (j < s.length()) {
+			if (!set.contains(s.charAt(j))) {
+				set.add(s.charAt(j++));
+				max = Math.max(max, set.size());
+			} else {
+				set.remove(s.charAt(i++));
+			}
+		}
+
+		return max;
+	}
+}
