79646b80d2db078c86f2bb9d389b7b662503f4f0
yugandvo <yugandvo@YUGANDVO01.mshome.net>
2020-08-28 19:46:58 +0530

    MeetingRooms

--- src/ds/io/LeetCode/MeetingRooms.java
+++ src/ds/io/LeetCode/MeetingRooms.java
@@ -17,11 +17,7 @@ public class MeetingRooms {
 	}
 
 	public static boolean canAttendMeetings(Interval[] intervals) {
-		Arrays.sort(intervals, new Comparator<Interval>(){
-			public int compare(Interval a, Interval b){
-				return a.start-b.start;
-			}
-		});
+		Arrays.sort(intervals, (a,b)-> a.start-b.start);
 
 		for(int i=0; i<intervals.length-1; i++){
 			if(intervals[i].end>intervals[i+1].start){
